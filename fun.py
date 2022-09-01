bebabd7b4f66af2ed47c8c169ee38a3bd0035198
SofiaFesenko <108746952+SofiaFesenko@users.noreply.github.com>
2022-09-01 16:19:12 +0300

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,14 @@
+# from random import*
+#
+# a = random()
+# b = random()
+#
+# def count_x_y(a, b):
+#     if a < b:
+#         a, b = b, a
+#     x_y_list = []
+#     for _ in range(2):
+#         x_y_list.extend([a, b])
+#     print(x_y_list[0], x_y_list[1])
+# count_x_y(a, b)
+#
---
+++ main.py
@@ -0,0 +1,15 @@
+# import matplotlib.pyplot as plt
+# from fun import*
+#
+# x = a
+# y = b
+#
+# plt.figure(figsize=(5, 2.7), layout='constrained')
+# plt.plot(x, y, label='linear')
+# plt.plot(x, x**2, label='quadratic')
+# plt.plot(x, x**3, label='cubic')
+# plt.xlabel('x label')
+# plt.ylabel('y label')
+# plt.title("Simple Plot")
+# plt.legend()
+# plt.show()
